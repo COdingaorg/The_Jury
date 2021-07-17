9c23b45c051209d664fef8472b173fc0baa607d4
Caleb Odinga <calemasanga@gmail.com>
2021-07-17 21:51:01 +0300

    adds views function for registering and loging in user

--- jury/views.py
+++ jury/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, redirect
 from .forms import registerUser
+from .models import User
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 def index(request):
@@ -13,9 +16,24 @@ def index(request):
 def register_user(request):
   form = registerUser
   title = 'Register-The Jury'
-
+  if request.method == 'POST':
+    form = registerUser(request.POST)
+    if form.is_valid():
+      uname = form.cleaned_data['username']
+      password = form.cleaned_data['password2']
+      form.save()
+      
+      to_login = authenticate(username = uname , password = password )
+      login(to_login)
+      
+      messages.success(request, 'Account Created Successfully!. Check out our Email later :)')
+      return redirect('/')
+    else:
+      messages.warning(request, 'invalid input')
+  else:
+    form = registerUser
   context = {
     'title':title,
     'form':form,
-  }  
+  }
   return render(request, 'django_registration/registration_form.html', context)
