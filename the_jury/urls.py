4d0d3c09f07ec2ec9f743c7ed67686a33cb93f94
Caleb Odinga <calemasanga@gmail.com>
2021-07-17 17:15:31 +0300

    adds navbar and fonts importation

--- the_jury/urls.py
+++ the_jury/urls.py
@@ -20,4 +20,6 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('jury.urls')),
+    path('accounts/', include('django_registration.backends.activation.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
