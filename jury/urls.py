5517ec184c8d52f0b3364f76d46cc77b7d45d15f
Caleb Odinga <calemasanga@gmail.com>
2021-07-17 20:06:59 +0300

    adds login template and styles it

--- jury/urls.py
+++ jury/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 from jury import views
 
-urlpatterns = {
+urlpatterns = [
   url(r'^$', views.index, name = 'home'),
   url(r'^register_user/$', views.register_user, name = 'register_user')
-}
+]
