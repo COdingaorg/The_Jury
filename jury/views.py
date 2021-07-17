0776baec9db2c5a87c607003a02329f2cfa8a89b
Caleb Odinga <calemasanga@gmail.com>
2021-07-17 14:56:42 +0300

    adds index template and view, create url file in app

--- jury/views.py
+++ jury/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+  title = 'The Jury-Home'
+
+  context = {
+    'title':title
+  }
+  return render(request, 'app_templates/index.html', context)
