31177adce5ad3372333f2b1ebabd9899cd12554b
Caleb Odinga <calemasanga@gmail.com>
2021-07-18 13:49:38 +0300

    test instance creation for user

--- jury/tests.py
+++ jury/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from .models import User
 
 # Create your tests here.
+class TestUser(TestCase):
+  '''
+  test case to test user methods, create user, delete user, update user, Get user
+  '''
+  def setUp(self):
+      self.new_user = User(username = 'Alfred', first_name = 'Alfred', last_name = 'Tito', email = 'tito@gmail.com')
+
+  def testinstance(self):
+    self.assertTrue(isinstance(self.new_user, User))
