ebfaa9ffee9e77bda8c81fc78d009c3cb7f49119
Caleb Odinga <calemasanga@gmail.com>
2021-07-18 13:19:33 +0300

    adds user profile model

--- jury/models.py
+++ jury/models.py
@@ -1,4 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.deletion import CASCADE
 
 # Create your models here.
+class UserProfile(models.Model):
+  photo_path = models.ImageField(upload_to = 'projects/')
+  user_bio = models.TextField(max_length=200)
+  facebook_account = models.TextField(max_length=100)
+  twitter_account = models.TextField(max_length=100)
+  instagram_account = models.TextField(max_length=100)
+  user = models.ForeignKey(User, on_delete=CASCADE)
+  
