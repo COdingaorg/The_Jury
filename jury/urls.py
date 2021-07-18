6b1efbf4ef5ffa3061e2b5bd34578977775c7d5f
Caleb Odinga <calemasanga@gmail.com>
2021-07-18 18:10:55 +0300

    adds search uaer template, view and url conf

--- jury/urls.py
+++ jury/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
   url(r'^$', views.index, name = 'home'),
   url(r'^register_user/$', views.register_user, name = 'register_user'),
   url(r'^upload_project/$', views.upload_project, name = 'upload_project'),
+  url(r'^search_user/$', views.get_user, name= 'get_user'),
 ]
--- jury/views.py
+++ jury/views.py
@@ -2,7 +2,7 @@ from django.contrib import messages
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
 from .forms import registerUser, UploadProjectForm
-from .models import User, UserProject
+from .models import User, UserProfile, UserProject
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 
@@ -43,6 +43,9 @@ def register_user(request):
 #A form to submit a users project for rating and reviews
 @login_required(login_url = 'login')
 def upload_project(request):
+  '''
+  A view function rendering to upload form template.
+  '''
   form = UploadProjectForm
   title = 'Submit Project - The Jury'
   if request.method == 'POST':
@@ -52,6 +55,8 @@ def upload_project(request):
       new_project_item.user = request.user
 
       new_project_item.save()
+
+      messages.success('Post Added Successfully!')
       
       return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
     else:
@@ -62,3 +67,29 @@ def upload_project(request):
       'form': form,
     }
     return render(request, 'app_templates/upload_project.html', context)
+
+#A search user view function
+def get_user(request):
+  '''
+  A view fuction responsible for returning searched user
+  '''
+  if 'search_term' in request.GET and request.GET['search_term']:
+    search_trm = request.GET.get('search_term')
+    user_profiles = User.objects.filter(username__icontains = search_trm)
+
+    context = {
+      'user_profiles':user_profiles,
+      'search_trm':search_trm
+    }
+
+    return render(request, 'app_templates/search_result.html', context)
+  
+  else:
+    title = 'Search Users'
+    message = 'No Users Found'
+    context = {
+      'message':message,
+      'title':title
+    }
+    
+    return render(request, 'app_templates/search_result.html', context)
